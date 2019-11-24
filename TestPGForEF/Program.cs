130ba8fb54321fca218ccda98d7e5c3836cad1d5
froomi <47034379+froomi@users.noreply.github.com>
2019-11-24 20:06:37 +0200

    Add files via upload

---
+++ TestPGForEF/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace TestPGForEF
+
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+               
+
+                Student student1 = new Student { Name = "Balko"};
+                Student student2 = new Student { Name = "Bestachenko" };
+                Student student3 = new Student { Name = "Zaiets" };
+                Student student4 = new Student { Name = "Shabatura" };
+                Student student5 = new Student { Name = "Yahnyschak" };
+                Student student6 = new Student { Name = "Yanishevsky" };
+
+              
+                    db.Students.Add(student1);
+                    db.Students.Add(student2);
+                    db.Students.Add(student3);
+                    db.Students.Add(student4);
+                    db.Students.Add(student5);
+                    db.Students.Add(student6);
+                db.SaveChanges();
+               Console.WriteLine("List is done successfully ");
+                
+
+                
+                var students = db.Students.ToList();
+                
+                Console.WriteLine("List of Students:");
+                foreach (Student s in students)
+                {
+                    Console.WriteLine($"{s.Id}.{s.Name} ");
+                }
+            }
+            Console.Read();
+        }
+    }
+}
